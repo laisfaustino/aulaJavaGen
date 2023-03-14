d9bda8bfbeb152cf705db4606aab750e53a23871
Lais Faustino <laisfaustino@hotmail.com>
2023-03-14 12:43:54 -0300

    Add files via upload

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int teste = 10;
+		System.out.println("Hello World");
+		System.out.println("\n Olá Turma 62!");
+		System.out.println("Variavel teste:"+ teste);
+	}
+
+}
